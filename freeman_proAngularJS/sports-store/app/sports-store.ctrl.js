12dda292c5751eb5b8eb3c668c0faee4fb58973c
Gary Grumbley <g.grumbley@alphasystem.lan>
2017-09-14 09:08:29 -0700

    Add thank you page

--- freeman_proAngularJS/sports-store/app/sports-store.ctrl.js
+++ freeman_proAngularJS/sports-store/app/sports-store.ctrl.js
@@ -1,20 +1,41 @@
 (function() {
-  'use strict';
+  "use strict";
 
   angular
-    .module('app')
-    .constant('ROOT_URL', 'http://localhost:3000')
-    .controller('SportsStoreCtrl', SportsStoreCtrl);
+    .module("app")
+    .constant("ROOT_URL", "http://localhost:3000")
+    .controller("SportsStoreCtrl", SportsStoreCtrl);
 
-  SportsStoreCtrl.$inject = ['$scope', '$http', 'ROOT_URL'];
-  function SportsStoreCtrl($scope, $http, ROOT_URL) {
+  SportsStoreCtrl.$inject = [
+    "$scope",
+    "$http",
+    "$state",
+    "ROOT_URL",
+    "CartService"
+  ];
+  function SportsStoreCtrl($scope, $http, $state, ROOT_URL, CartService) {
     const vm = this;
 
-    vm.data = {}
+    vm.data = {};
 
     $http
       .get(`${ROOT_URL}/products`)
-      .then((res) => vm.data.products = res.data, (err) => vm.data.error = err.data);
+      .then(
+        res => (vm.data.products = res.data),
+        err => (vm.data.error = err.data)
+      );
 
-  };
+    vm.sendOrder = shippingDetails => {
+      let order = angular.copy(shippingDetails);
+      order.products = CartService.getProducts();
+      console.log(order);
+      $http.post(`${ROOT_URL}/orders`).then((res) => console.log(res.data))
+      // $http.post(`${ROOT_URL}/orders`).then(res => {
+      //   console.log(res.data)
+      //   // vm.data.orderId = res.data.id;
+      //   // CartService.getProducts().length = 0;
+      // }, err => (vm.data.orderError = err.data))
+      // // .then(() => $state.go('complete'));
+    };
+  }
 })();
